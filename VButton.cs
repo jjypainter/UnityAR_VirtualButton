1abeda654f2cd0a7c9fa0bbb0850781181b9ed48
jjypainter <jypainter@naver.com>
2020-01-21 23:14:17 +0900

    스크립트 작성

--- VButton.cs
+++ VButton.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class VButton : MonoBehaviour
+public class VButton : MonoBehaviour, IVirtualButtonEventHandler
 {
+    VirtualButtonBehaviour[] virtualBtnBehaviours;
+    //public GameObject warning
     // Start is called before the first frame update
     void Start()
     {
+        virtualBtnBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < virtualBtnBehaviours.Length; ++i)
+        {
+            virtualBtnBehaviours[i].RegisterEventHandler(this);
+        }
         
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Debug.Log("OnButtonPressed: " + vb.VirtualButtonName);
+        GetComponentInChildren<Animator>().SetBool("isAtteck", true);
+        //warning.SetActive(true);
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased: " + vb.VirtualButtonName);
+        GetComponentInChildren<Animator>().SetBool("isAtteck", false);
     }
 
     // Update is called once per frame
